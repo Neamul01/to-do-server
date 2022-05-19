e492419e22b0263764369b2c9a3f61869fa14449
Neamul01 <neamulhaque2002@gmail.com>
2022-05-19 07:40:23 +0600

    api added

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const cors = require('cors');
+const res = require('express/lib/response');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -11,6 +13,31 @@ app.use(express.json());
 app.get('/', (req, res) => {
     res.send('server is running...!!!')
 })
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.rcf71.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect();
+        const listCollection = client.db('to-do').collection('list');
+
+        app.get('/list', async (req, res) => {
+            const query = {};
+            const result = await listCollection.find(query).toArray();
+            res.send(result);
+        });
+        app.post('/list', async (req, res) => {
+            const data = req.body;
+            const result = await listCollection.insertOne(data);
+            res.send(result)
+        })
+    }
+    finally {
+
+    }
+}
+run().catch(console.dir)
 
 app.listen(port, () => {
     console.log('app running at port: ', port)
